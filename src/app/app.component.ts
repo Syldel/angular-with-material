97a998030ec803438bb176cc58b2b34b4e59c788
Syldel <syldeles@hotmail.fr>
2020-05-15 15:44:34 +0200

    refactor: remove useless things

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { environment } from './../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +8,6 @@ import { environment } from './../environments/environment';
 export class AppComponent {
   title = 'angular-project';
 
-  constructor() {
-    console.log('AppComponent environment.apiUrl:', environment.apiUrl);
-  }
+  constructor() { }
+
 }
--- src/app/not-found-page/not-found-page.module.ts
+++ src/app/not-found-page/not-found-page.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { NotFoundPageRoutingModule } from './not-found-page-routing.module';
 
 @NgModule({
   imports: [
-    CommonModule,
     NotFoundPageRoutingModule
   ]
 })
